8c60f6481515c782d33160ddee6299adc670d6b1
Pawlo010 <andrijmartunyak@gmail.com>
2020-01-13 17:23:42 +0200

    Test 2

---
+++ Calculation/Colculation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    public class Colculation
+    {
+        int Sum { get; set; }
+        public Colculation(int a, int b)
+        {
+            this.Sum = a + b;
+        }
+    }
+}
